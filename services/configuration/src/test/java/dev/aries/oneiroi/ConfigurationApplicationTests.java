229242ffd6a2ee96f9ee82baba6934a4159423c7
Jacobs Agyei <jacobs.agyei@amalitech.com>
2024-10-07 15:00:57 +0000

    build(configuration): initial commit

---
+++ services/configuration/src/test/java/dev/aries/oneiroi/ConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aries.oneiroi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
