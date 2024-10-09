aec86faf93c12ae113120294c3bd7a90fb8703ea
Jacobs Agyei <agyeijacobs@outlook.com>
2024-10-09 16:06:47 +0000

    feat(hr): add employee management service

---
+++ services/hr/src/main/java/dev/aries/oneiroi/HRApplication.java
@@ -0,0 +1,17 @@
+package dev.aries.oneiroi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableFeignClients
+public class HRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HRApplication.class, args);
+	}
+
+}
