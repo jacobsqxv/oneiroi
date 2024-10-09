9165a426edc5d2d8628863490c79bb2b39c9678e
Jacobs Agyei <agyeijacobs@outlook.com>
2024-10-09 20:17:50 +0000

    feat(hr): add rest endpoints for service implementation

---
+++ services/hr/src/main/java/dev/aries/oneiroi/controller/DepartmentController.java
@@ -0,0 +1,60 @@
+package dev.aries.oneiroi.controller;
+
+import java.util.List;
+
+import dev.aries.oneiroi.dto.DepartmentResponse;
+import dev.aries.oneiroi.dto.DepartmentRequest;
+import dev.aries.oneiroi.model.Department;
+import dev.aries.oneiroi.service.departmentservice.DepartmentService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/departments")
+@RequiredArgsConstructor
+public class DepartmentController {
+
+	private final DepartmentService departmentService;
+
+	//	Used by other microservices to validate a department by id
+	@GetMapping("/validate")
+	public ResponseEntity<Department> validateDeptId(@RequestParam Integer id) {
+		return ResponseEntity.ok(departmentService.getDepartment(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<DepartmentResponse> addNewDepartment(@RequestBody DepartmentRequest request) {
+		return new ResponseEntity<>(departmentService.addNewDepartment(request), HttpStatus.CREATED);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<DepartmentResponse>> getAllDepartments() {
+		return ResponseEntity.ok(departmentService.getAllDepartments());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<DepartmentResponse> getDepartment(@PathVariable Integer id) {
+		return ResponseEntity.ok(departmentService.getDepartmentResponse(id));
+	}
+
+	@PutMapping("/{id}/info")
+	public ResponseEntity<DepartmentResponse> updateDepartmentInfo(@PathVariable Integer id, @RequestBody DepartmentRequest request) {
+		return ResponseEntity.ok(departmentService.updateInfo(id, request));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteDepartment(@PathVariable Integer id) {
+		return new ResponseEntity<>(departmentService.deleteDepartment(id), HttpStatus.NO_CONTENT);
+	}
+}
---
+++ services/hr/src/main/java/dev/aries/oneiroi/controller/EmployeeController.java
@@ -0,0 +1,73 @@
+package dev.aries.oneiroi.controller;
+
+import java.util.List;
+
+import dev.aries.oneiroi.dto.EmployeeRequest;
+import dev.aries.oneiroi.dto.EmployeeResponse;
+import dev.aries.oneiroi.dto.PromotionRequest;
+import dev.aries.oneiroi.dto.TransferRequest;
+import dev.aries.oneiroi.model.Employee;
+import dev.aries.oneiroi.service.employeeservice.EmployeeService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+	private final EmployeeService employeeService;
+
+	//	Used by other microservices to validate an employee by id
+	@GetMapping("/validate")
+	public ResponseEntity<Employee> validateEmployeeId(@RequestParam Integer id) {
+		return ResponseEntity.ok(employeeService.getEmployee(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<EmployeeResponse> addNewEmployee(@RequestBody EmployeeRequest request) {
+		return new ResponseEntity<>(employeeService.addNewEmployee(request), HttpStatus.CREATED);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<EmployeeResponse>> getAllEmployees() {
+		return ResponseEntity.ok(employeeService.getAllEmployees());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<EmployeeResponse> getEmployeeResponse(@PathVariable Integer id) {
+		return ResponseEntity.ok(employeeService.getEmployeeResponse(id));
+	}
+
+	@PutMapping("/{id}/info")
+	public ResponseEntity<EmployeeResponse> updateEmployeeInfo(@PathVariable Integer id, @RequestBody EmployeeRequest request) {
+		return ResponseEntity.ok(employeeService.updateEmployeeInfo(id, request));
+	}
+
+	@PutMapping("/promotions")
+	public ResponseEntity<List<EmployeeResponse>> promoteEmployee(@RequestBody PromotionRequest request) {
+		return ResponseEntity.ok(employeeService.promoteEmployees(request));
+	}
+
+	@PutMapping("/transfers")
+	public ResponseEntity<List<EmployeeResponse>> transferEmployees(@RequestBody TransferRequest request) {
+		return ResponseEntity.ok(employeeService.transferEmployees(request));
+	}
+
+	@DeleteMapping("/terminate")
+	public ResponseEntity<Void> terminateEmployees(@RequestBody List<Integer> employeeIds) {
+		return new ResponseEntity<>(employeeService.terminateEmployees(employeeIds),
+				HttpStatus.NO_CONTENT);
+	}
+}
