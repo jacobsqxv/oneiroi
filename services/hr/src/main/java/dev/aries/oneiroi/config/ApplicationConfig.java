0c237ddd35e1519e42e5921d2b0f6c33b40e4b7d
Jacobs Agyei <agyeijacobs@outlook.com>
2024-10-14 14:53:38 +0000

    build(hr): update configuration

---
+++ services/hr/src/main/java/dev/aries/oneiroi/config/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package dev.aries.oneiroi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class ApplicationConfig {
+
+}
